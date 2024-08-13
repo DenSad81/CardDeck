4f83508619f1d7d91a348feb35bed043ba22d880
DenSad81 <tagil81@mail.ru>
2024-08-13 15:44:56 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -8,15 +8,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Desk desk = new Desk(new List<Card>() {new Card("bubi", 6),
-                                               new Card("vini", 7),
-                                               new Card("bubi", 8),
-                                               new Card("vini", 9),
-                                               new Card("bubi", 11),
-                                               new Card("vini", 12),
-                                               new Card("bubi", 14),
-                                               new Card("vini", 19)});
+        Desk desk = new Desk();
         Player player = new Player("Jonn");
+        Diller diller = new Diller();
 
         ShowSituation(desk, player);
 
@@ -24,7 +18,14 @@ class Program
         int quantityOfCards = GetIntFromConsole();
 
         for (int i = 0; i < quantityOfCards; i++)
-            player.AcceptCard(desk.GiveCard());
+        {
+            if (diller.TryGiveCard(out Card card))
+                player.AcceptCard(card);
+        }
+
+        while (diller.TryGiveCard(out Card card))
+            desk.AcceptCard(card);
+
 
         ShowSituation(desk, player);
     }
@@ -50,29 +51,6 @@ class Program
     }
 }
 
-class Desk
-{
-    private List<Card> _cards;
-    public Desk(List<Card> cards)
-    {
-        _cards = cards;
-    }
-
-    public void ShowAllCards()
-    {
-        foreach (var card in _cards)
-            card.Show();
-    }
-
-    public Card GiveCard()
-    {
-        Card tempCard;
-        tempCard = _cards[0];
-        _cards.RemoveAt(0);
-        return tempCard;
-    }
-}
-
 class Card
 {
     private string _suit;
@@ -90,14 +68,53 @@ class Card
     }
 }
 
+class Desk
+{
+    private List<Card> _cards = new List<Card>();
+
+    public void ShowAllCards()
+    {
+        foreach (var card in _cards)
+            card.Show();
+    }
+
+    public void AcceptCard(Card card)
+    {
+        _cards.Add(card);
+    }
+}
+
+class Diller
+{
+    private List<Card> _cards = new List<Card>() {new Card("bubi", 6),
+                                                  new Card("vini", 7),
+                                                  new Card("bubi", 8),
+                                                  new Card("vini", 9),
+                                                  new Card("bubi", 11),
+                                                  new Card("vini", 12),
+                                                  new Card("bubi", 14),
+                                                  new Card("vini", 19)};
+
+    public bool TryGiveCard(out Card card)
+    {
+        card = null;
+
+        if (_cards.Count == 0)
+            return false;
+
+        card = _cards[0];
+        _cards.RemoveAt(0);
+        return true;
+    }
+}
+
 class Player
 {
-    private List<Card> _cards;
-    private string _name;
+    private List<Card> _cards = new List<Card>();
+    private string _name = "no name";
 
-    public Player(string name = "no name")
+    public Player(string name)
     {
-        _cards = new List<Card>();
         _name = name;
     }
 
